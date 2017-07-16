e03e93d9148baa55b88b2cd663798ca1f4b8a1bf
Dani Rosás <DaniRosas@MacBook-Pro-de-Dani.local>
2017-07-16 18:28:16 +0200

    first commit

---
+++ CameraSimple/app/src/main/java/dani/rosas/camera/MainActivity.java
@@ -0,0 +1,59 @@
+package dani.rosas.camera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Button button;
+    private ImageView imageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button    = (Button) findViewById(R.id.openCameraButton);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        button.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.openCameraButton:
+
+                dispatchTakePictureIntent();
+
+                break;
+        }
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+}
---
+++ CameraSimple/app/src/main/java/dani/rosas/camera/entities/Photo.java
@@ -0,0 +1,10 @@
+package dani.rosas.camera.entities;
+
+/**
+ * Created by DaniRosas on 16/7/17.
+ */
+
+public class Photo {
+    private String url;
+    private String email;
+}
